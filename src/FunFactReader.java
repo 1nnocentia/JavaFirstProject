6052b6021cefa7277a35200c38563ae6b3de7531
Innocentia <innocentiahan@gmail.com>
2025-02-27 11:23:25 +0800

    Memperbaiki Json reader

--- src/FunFactReader.java
+++ src/FunFactReader.java
@@ -1,12 +1,14 @@
 import com.google.gson.Gson; //https://mvnrepository.com/artifact/com.google.code.gson/gson/2.12.1
+import com.google.gson.reflect.TypeToken;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class FunFactReader {
-    private String filePath;
+    private String filePath; //Read filepath from variable in Main
 
     public FunFactReader(String filePath) {
         this.filePath = filePath;
@@ -14,8 +16,8 @@ public class FunFactReader {
 public String getRandomFact() {
     Gson gson = new Gson();
     try (FileReader reader = new FileReader(filePath)) {
-        // Parse JSON menjadi Map
-        Map<String, List<String>> data = gson.fromJson(reader, Map.class);
+        Type type = new TypeToken<Map<String, List<String>>>() {}.getType();     
+        Map<String, List<String>> data = gson.fromJson(reader, type);
         List<String> funfacts = data.get("funfacts");
 
         if (funfacts != null && !funfacts.isEmpty()) {
